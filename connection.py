53545e538d680d86125848852983bb08ba15d1e7
jean-christophe bauduin <maildejc@hotmail.be>
2017-07-31 10:13:57 +0200

    fin

--- connection.py
+++ connection.py
@@ -3,15 +3,28 @@
 # -*- coding: utf-8 -*-
 import time
 import serial
+import urllib2
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
-base = "http://localhost/LoadDatas?data=true"
+base = "http://localhost:8000/LoadDatas?data=true"
 data =""
 debut = "non-defini"
-derniere_maj = 0
+
+derniere_maj=0
 isline=False
-def sendData(data):
-	print data
-	return True
+def sendData(data,m):
+	global derniere_maj
+	if derniere_maj+(60) < m:
+		url = base+data
+		try:
+			response = urllib2.urlopen(url)
+			html = response.read()
+			derniere_maj = m
+			print html
+			return True
+		except:
+			print "erreur"
+			return False
 while 1 :
 	line = ser.readline()
 	line = line.strip()
@@ -26,17 +39,14 @@ while 1 :
 
 	elif line=="fin":
 		debut = False
-		sendData(data)
+		sendData(data,m)
 		isline=True
 
 	try:
 		if line[0] != "#" and debut != "non-defini" and isline==False:
 			line = line.split(":")
-			derniere_maj = m
 			data +="&"+line[0]+"="+str(line[1])
 	except:
 		debut  = "non-defini"
 		data = ""
-		print line
-		print "erreur"
 
